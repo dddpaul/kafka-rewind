73a5add513c3dc2ba5bb021ba18201db6e4f49f2
Pavel Derendyaev <pderendyaev@alfabank.ru>
2018-03-10 11:30:03 +0300

    Test refuck

--- src/test/java/com/github/dddpaul/kafka/rewind/ApplicationTest.java
+++ src/test/java/com/github/dddpaul/kafka/rewind/ApplicationTest.java
@@ -6,14 +6,19 @@ import com.github.charithe.kafka.KafkaJunitExtensionConfig;
 import com.github.charithe.kafka.StartupMode;
 import org.apache.commons.configuration2.MapConfiguration;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import picocli.CommandLine;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import static java.time.format.DateTimeFormatter.ISO_DATE;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.apache.commons.configuration2.ConfigurationConverter.getProperties;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,37 +31,49 @@ class ApplicationTest {
     private static final Properties PROPS = getProperties(new MapConfiguration(Map.of(
             "group.id", GROUP_ID
     )));
+    private static final String TODAY = ISO_DATE.format(LocalDate.now());
+    private static final String TOMORROW = ISO_DATE.format(LocalDate.now().plusDays(1));
 
     @Test
     void test(KafkaHelper kafkaHelper) throws Exception {
         // given
         kafkaHelper.produceStrings(TOPIC, "a", "b", "c", "d", "e");
 
-        // and
-        List<ConsumerRecord<String, String>> records =
-                kafkaHelper.consume(TOPIC, kafkaHelper.createStringConsumer(PROPS), 5).get();
-        assertThat(records).hasSize(5);
+        // and consume all
+        assertThat(consume(kafkaHelper, 5)).hasSize(5);
 
-        // and
+        // and ensure there is nothing to consume
+        assertThat(consume(kafkaHelper, 1)).isEmpty();
+
+        // and rewind to the start of the day
         CommandLine.populateCommand(new Application(),
                 "--servers=localhost:" + kafkaHelper.kafkaPort(),
                 "--group-id=" + GROUP_ID,
                 "--topic=" + TOPIC,
-                "--offset=0=2017-12-01").start();
+                "--offset=0=" + TODAY).start();
 
-        // then with the same group id
-        records = kafkaHelper.consume(TOPIC, kafkaHelper.createStringConsumer(PROPS), 5).get();
-        assertThat(records).hasSize(5);
+        // then consume all
+        assertThat(consume(kafkaHelper, 5)).hasSize(5);
 
-        // and
+        // and ensure there is nothing to consume
+        assertThat(consume(kafkaHelper, 1)).isEmpty();
+
+        // and rewind to the next day
         CommandLine.populateCommand(new Application(),
                 "--servers=localhost:" + kafkaHelper.kafkaPort(),
                 "--group-id=" + GROUP_ID,
                 "--topic=" + TOPIC,
-                "--offset=0=2017-12-30").start();
+                "--offset=0=" + TOMORROW).start();
 
-        // then with the same group id
-        records = kafkaHelper.consume(TOPIC, kafkaHelper.createStringConsumer(PROPS), 0).get();
-        assertThat(records).isEmpty();
+        // then consume nothing
+        assertThat(consume(kafkaHelper, 1)).isEmpty();
+    }
+
+    private List<ConsumerRecord<String, String>> consume(KafkaHelper kafkaHelper, int amount) {
+        try (KafkaConsumer<String, String> consumer = kafkaHelper.createStringConsumer(PROPS)) {
+            return kafkaHelper.consume(TOPIC, consumer, amount).get(5, SECONDS);
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
     }
 }
